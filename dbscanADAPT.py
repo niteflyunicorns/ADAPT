e373eb78694c96feb294ce365eaff4fb73b4b8f1
niteflyunicorns <chappus.savannah@gmail.com>
2025-02-13 10:04:09 -0700

    initial file for DBSCAN

---
+++ dbscanADAPT.py
@@ -0,0 +1,21 @@
+#########################################################################################
+### Program: ADAPT (Anomaly Detection for Asteroid Patterns and Trends)
+### Programmer: Savannah Chappus
+### Last Update: 2.7.2025
+###
+### File: mongoConnection.py
+### Use: handles logic for connecting to the mongo database for ZTF data
+#########################################################################################
+
+## IMPORTS ##############################################################################
+from mongoConnection import Mongo
+#scikitlearn
+
+
+
+
+
+# Notes:
+# must be ablet to take any number of parameters
+# takes in unfiltered, untrimmed data for processing
+# smallest cluster at end is the outlier cluster
