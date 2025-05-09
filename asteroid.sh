5b88cc1573392b46db6187f7f9072d5f5df1ca55
niteflyunicorns <chappus.savannah@gmail.com>
2025-05-09 09:38:43 -0700

    started adding functionality to pass file of specific asteroid names into the program. Currently this functionality does not work, unsure whether we will implement it fully in the future or not.

--- asteroid.sh
+++ asteroid.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=snaps  #the name of your job
+#SBATCH --job-name=adapt  #the name of your job
 
 #change to your NAUID
-#SBATCH --output=/scratch/sjc497/astMat.out #this is the file for stdout 
-#SBATCH --error=/scratch/sjc497/astMat.err #this is the file for stderr
+#SBATCH --output=/scratch/sjc497/adapt-isoforest-timing100New2.out #this is the file for stdout 
+#SBATCH --error=/scratch/sjc497/adapt-isoforest-timing100New2.err #this is the file for stderr
 
-#SBATCH --time=00:10:00        #Job timelimit is 4 hours
-#SBATCH --mem=8GB        #memory requested in MiB
+#SBATCH --time=00:10:00
+#SBATCH --mem=8GB
 #SBATCH --cpus-per-task=1
 
 # VARIABLES ---------------------------------------
 # required variables:
 maxIn=1
-offset=3289
+offset=1000
+# fromFileFlg=False
+# file="/home/sjc497/ADAPT/astNames.csv"
 # attrs=['elong', 'mag18omag8', 'H', 'rb']
 
-fltrType='mix'
+fltrType='isoforest'
 # ^ fltrType can be one of:
 # 'anomaly', 'dbscan', 'isoforest', 'mix'
 fltrLvl=2
@@ -25,17 +27,20 @@ exportFlg=True
 
 # optional variables:
 fileType=2 # default 2 (.csv)
-fileName="~/astroInfoResearch/astroInfo/ast291032" # default ""
-astName=2652 # default 0
+fileName="/scratch/sjc497/ADAPT/data/" # default ""
+astName=2156 # default 0
 featFltr=n # default n
 lB=0 # default 0
 uB=0 # default 0
 
 
 
-# PRE-RUN CLEANUP
+# PRE-RUN STUFF
 # remove output files from previous run
 # rm -f /scratch/sjc497/astMat.*
+# load conda in case it's not loaded already
+# module load anaconda3
+# conda activate astroEnv
 
 
 # PROFILING ---------------------------------------
@@ -43,13 +48,12 @@ uB=0 # default 0
 # time kernprof -lv runADAPT.py "$maxIn" "$offset" "$fltrType" "$fltrLvl" "$plots" "$exportFlg"
 
 
-
 # RUNNING -----------------------------------------
 # No export, multiple asteroids
 if [ $exportFlg == "False" ]; then
 	if [ $maxIn -gt 1 ]; then
-	   # No export, multiple asteroids
-	   time python runADAPT.py "$maxIn" "$offset" "$fltrType" "$fltrLvl" "$plots" "$exportFlg"
+		# No export, multiple asteroids
+		time python runADAPT.py "$maxIn" "$offset" "$fltrType" "$fltrLvl" "$plots" "$exportFlg" "$fromFileFlg"
 	else
 	   # No export, single asteroid
 	   time python runADAPT.py "$maxIn" "$offset" "$fltrType" "$fltrLvl" "$plots" "$exportFlg" "$astName" "$featFltr" "$lB" "$uB"
@@ -63,6 +67,11 @@ else
 		time python runADAPT.py "$maxIn" "$offset" "$fltrType" "$fltrLvl" "$plots" "$exportFlg" "$fileType" "$fileName" "$astName" "$featFltr" "$lB" "$uB"
     fi
 fi
+
+
+
+# if [ $fromFileFlg == "True" ]; then
+# time python runADAPT.py "$maxIn" "$offset" "$fltrType" "$fltrLvl" "$plots" "$exportFlg" "$fromFileFlg" "$fileType" "$fileName" "$file"
 
 # if [$(maxIn) == -1]; then
 # fi
