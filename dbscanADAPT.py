03e52498fac0a062d2f8aa801c54e2ebe0563e37
niteflyunicorns <chappus.savannah@gmail.com>
2025-05-09 09:42:09 -0700

    Changes included in this commit are as follows: * Added functionality for getting specific interesting clusters for output, which was originally implemented in hybridADAPT.py * Modified parameter tuning function to be able to only output one "best" version at the end * Other miscellaneous cleanup related to matching the latest hybrid functionality and plots.

--- dbscanADAPT.py
+++ dbscanADAPT.py
@@ -32,22 +32,46 @@ def preProcess( astData, name ): # will take cols as input later
     scaler = Normalizer()
     data = pd.DataFrame( astData.mag18.find( { "ssnamenr": int( name ) } ) )
     sortedDF = astData.sort( data, "jd" )
-    df = astData.trimToCols( sortedDF, ["rb", "elong", "mag18omag8"] )
+    df = astData.trimToCols( sortedDF, ["elong", "rb", "mag18omag8"] )
     dataArray = df.to_numpy()
     normData = scaler.fit_transform( dataArray )
     return sortedDF, dataArray, normData
-    
+
+
+def getClusters( data, labels ):
+    clusterLabels = []
+    maxSize = 1
+    maxClusterLabel = 0
+    for k in labels:
+        cluster = data[ labels == k ]
+        if len( cluster ) > maxSize:
+            maxSize = len( cluster )
+            maxClusterLabel = k
+        if k not in clusterLabels and k != -1:
+            clusterLabels.append( k )
+    if maxClusterLabel in clusterLabels:
+        clusterLabels.remove( maxClusterLabel )
+
+    return clusterLabels
+
+
+def fetchDataForCluster( clusterNum, data, labels, name, cols, exportFlg ):
+    clusterData = data[ labels == clusterNum ]
+    data = getObs.getSelect( name, clusterData, data, cols, exportFlg )
+    return data
+
 
 # data needs to be trimmed to desiredCols (1 asteroid at a time)
-def runDBSCAN( astData, plots, export ): # not sure if eps and minPts should be passed here or calculated later
+def runDBSCAN( astData, plots, exportFile, export ): # not sure if eps and minPts should be passed here or calculated later
     # attemting to modify this to run multiple also:
     names = astData.names
     for astName in names:
-        # astName = astData.names[0] # for now
         untrimmed, unnorm, data = preProcess( astData, astName )
 
         e = 0.04 # starting point
         minPts = 3 # starting point
+        # e = 8.0
+        # minPts = 5 # 5 or 7
 
         tune = False
         stamps = False
@@ -63,33 +87,57 @@ def runDBSCAN( astData, plots, export ): # not sure if eps and minPts should be 
                 extraStuff = [ minPts, e, clusterSizes ]
                 plotDBSCAN( labels, db, extraStuff, unnorm, astName, export )
 
-            clusterData = fetchDataForCluster( 1, untrimmed, labels, astName, astData.dataCols, export )
+            getObs.getAll( astName, untrimmed, astData.dataCols, exportFile + "dbscan/", export )
+            
+            COI = getClusters( untrimmed, labels )
+            for cluster in COI:
+                clusterData = fetchDataForCluster( cluster, untrimmed, labels, astName, astData.dataCols, export )
+                if ( export ):
+                    filename = exportFile + "dbscan/" + str(astName) + "-dbscan-cluster" + str(cluster)
+                    out.exportFile( 3, filename, clusterData[ astData.dataCols ] )
+                else:
+                    out.screenDisplay( clusterData[ astData.dataCols ], "Cluster " + str( cluster ) + " Data" )
 
-            getObs.getAll( astName, untrimmed, astData.dataCols, export )
-            if ( export ):
-                out.exportFile( 3, str(astName) + "clusterData", clusterData )
-            else:
-                out.screenDisplay( clusterData, "Cluster Data" )
-
-            if ( stamps ):
-                postage.fromDF( clusterData )
+            # if ( stamps ):
+            #     postage.fromDF( clusterData )
 
 
 def paramTune( astData, astName, plots, export ):
-    # astName = astData.names[0] # for now
-    untrimmed, unnorm, data = preProcess( astData )
-
-    for e in float_range( 0.01, 0.1, 0.01 ):
-        for minPts in range( 3, 10, 2):
-            db = DBSCAN(eps=e, min_samples=minPts ).fit( data )
+    maxScore, goodE, goodPts = 0, 0, 0
+    maxScore = 0
+    untrimmed, unnorm, data = preProcess( astData, astName )
+    for e in float_range( 5, 10.5, 0.5 ): # unnormalized range
+    # for e in float_range( 0.05, 1.0, 0.05 ):
+        for minPts in range( 5, 9, 2 ):
+            score = 0
+            db = DBSCAN(eps=e, min_samples=minPts ).fit( unnorm )
             labels = db.labels_
             clusters = db.fit_predict(data)
             clusterSizes = Counter( clusters )
 
-            if ( plots ):
-                extraStuff = [ minPts, e, clusterSizes ]
-                plotDBSCAN( labels, db, extraStuff, unnorm, astName, export )
+            noise = list( labels ).count( -1 )
+            noisePercent = ( noise / len( labels ) ) * 100
+            n_clusters = len( set( labels ) ) - ( 1 if -1 in labels else 0 )
+            # score results so that we only output one result. 
+            if 2 <= n_clusters <= 10:
+                score += 2
+            elif 10 < n_clusters <= 20:
+                score += 1
+            else:
+                score -= 1
 
+            if 5.0 <= noisePercent <= 30.0:
+                score += 2
+            else:
+                score -= 1
+
+            if score > maxScore:
+                maxScore = score
+                goodE = e
+                goodPts = minPts
+    if ( plots ) and goodE != 0 and goodPts != 0:
+        extraStuff = [ goodPts, goodE, clusterSizes ]
+        plotDBSCAN( labels, db, extraStuff, unnorm, astName, export )
         
 
 def plotDBSCAN( labels, db, extras, data, astName, export ):
@@ -141,16 +189,16 @@ def plotDBSCAN( labels, db, extras, data, astName, export ):
             legendEntries.append( patch )
 
 
-    ax.set_xlabel( "rb" )    
-    ax.set_ylabel( "elong" )
+    ax.set_xlabel( "elong" )    
+    ax.set_ylabel( "rb" )
     ax.set_zlabel( "mag18omag8" )
     ax.set_title(f"{astName}")
 
     ax.set_xlim(np.min(data[:, 0]), np.max(data[:, 0]))
     ax.set_ylim(np.min(data[:, 1]), np.max(data[:, 1]))
     ax.set_zlim(np.min(data[:, 2]), np.max(data[:, 2]))
-    ax.invert_yaxis()
-    ax.invert_xaxis()
+    # ax.invert_yaxis()
+    # ax.invert_xaxis()
     # ax.invert_zaxis()
 
     noisePercent = ( n_noise_ / len( labels ) ) * 100
@@ -178,8 +226,8 @@ def plotDBSCAN( labels, db, extras, data, astName, export ):
     
 
     if export:
-        ext = "(" + str(minPts) + "-" + str(e) + ")"
-        filePath = "/home/sjc497/ADAPT/pngs/"
+        ext = str(minPts) + "-" + str(e)
+        filePath = "/scratch/sjc497/ADAPT/pngs/dbscan/"
         fig.savefig( filePath + str(astName) + "dbscan" + ext + ".png" )
         plt.close()
     else:
@@ -189,15 +237,3 @@ def plotDBSCAN( labels, db, extras, data, astName, export ):
         plt.show( block=True )
         fig.show()
 
-
-
-def fetchDataForCluster( clusterNum, data, labels, name, cols, exportFlg ):
-    clusterData = data[ labels == clusterNum ]
-    data = getObs.getSelect( name, clusterData, data, cols, exportFlg )
-    return data
-
-
-# Notes:
-# must be able to take any number of parameters
-# takes in unfiltered, untrimmed data for processing
-# smallest cluster at end is the outlier cluster
